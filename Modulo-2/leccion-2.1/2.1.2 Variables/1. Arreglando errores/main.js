8d8892367121e0e8ea80c8a0ca34d4fc96988db7
Nuria Campo <nuria.campo7@gmail.com>
2024-10-12 11:11:21 +0200

    Modulo 2 ejercicio 2.1.2-1 Arreglando errores

---
+++ Modulo-2/leccion-2.1/2.1.2 Variables/1. Arreglando errores/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Poniendo const sale un error en la consola:
+
+// const myaddress = 'Madrid, 28009';
+// myaddress = 'Valladolid, 47009';
+
+// Si lo cambiamos por let, ya no sale el error:
+
+let myaddress = 'Madrid, 28009';
+myaddress = 'Valladolid, 47009';
