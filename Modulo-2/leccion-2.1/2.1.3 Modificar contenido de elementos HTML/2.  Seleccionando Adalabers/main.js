c85ade84f3f7d5fc661bc426ee2507e5b897be07
Nuria Campo <nuria.campo7@gmail.com>
2024-10-12 11:48:04 +0200

    Modulo 2 ejercicio 2.1.3-2 Seleccionando Adalabers

---
+++ Modulo-2/leccion-2.1/2.1.3 Modificar contenido de elementos HTML/2.  Seleccionando Adalabers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const title = document.querySelector('.title');
+
+const selectAdalaber1 = document.querySelector('.adalaber-1');
+
+title.innerHTML = title.innerHTML + selectAdalaber1.innerHTML;
+
+
+// Una vez hecho esto, cambia el código para que el nombre sea el del segundo li
+
+
+// let selectAdalaber2 = document.querySelector('.adalaber-2');
+
+
+// title.innerHTML = title.innerHTML + selectAdalaber2.innerHTML;
