ee1bc272b4f4269a1f661a737fed731df9967b8d
Nuria Campo <nuria.campo7@gmail.com>
2024-10-13 13:14:25 +0200

    Modulo 2 ejercicios 2.2.3-2 Estilo para un botón NO FUNCIONA

---
+++ Modulo-2/leccion-2.2/2.2.3 Información de eventos/2. Estilo para un boton NO FUNCIONA/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Con una función para cada botón, funciona:
+
+const btn1 = document.querySelector('.btn-1');
+
+const btn2 = document.querySelector('.btn-2');
+
+btn1.addEventListener("click", () => {
+  btn1.classList.toggle('btn-click');
+});
+
+btn2.addEventListener("click", () => {
+  btn2.classList.toggle('btn-click');
+});
+
+
+// Con una función para los dos botones, NO funciona:
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener("click", (event) => {
+//   console.log('hago click');
+
+//   const btnClick = event.currentTarget;
+//   btnClick.classList.toggle('btn-click');
+// })
