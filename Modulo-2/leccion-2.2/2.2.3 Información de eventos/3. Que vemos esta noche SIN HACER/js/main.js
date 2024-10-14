96a247e39db810d8b1abc5dc156e538be45bdf83
Nuria Campo <nuria.campo7@gmail.com>
2024-10-14 18:08:16 +0200

    Modulo 2 ejercicios 2.2.3-3 Que vemos esta noche

--- Modulo-2/leccion-2.2/2.2.3 Información de eventos/3. Que vemos esta noche SIN HACER/js/main.js
+++ Modulo-2/leccion-2.2/2.2.3 Información de eventos/3. Que vemos esta noche SIN HACER/js/main.js
@@ -10,18 +10,40 @@ const split = 'Split';
 
 const btn = document.querySelector('.js-btn');
 const list = document.querySelector('.js-list');
+const p = document.querySelector('.js-paragraph');
 
 // Cuando hago click en el botón, añade a list todos los <li>
 btn.addEventListener("click", () => {
   console.log("Hago click");
-  list.innerHTML = `<li class="movie">${inception}</li> <li>${theButterFlyEffect}</li> <li>${eternalSunshineOfTheSM}</li> <li>${blueVelvet}</li> <li>${split}</li>`;
+  list.innerHTML = `<li class="movie-1">${inception}</li> <li class="movie-2">${theButterFlyEffect}</li> <li class="movie-3">${eternalSunshineOfTheSM}</li> <li class="movie-4">${blueVelvet}</li> <li class="movie-5">${split}</li>`;
+  
+  const movie1 = document.querySelector('.movie-1');
+  const movie2 = document.querySelector('.movie-2');
+  const movie3 = document.querySelector('.movie-3');
+  const movie4 = document.querySelector('.movie-4');
+  const movie5 = document.querySelector('.movie-5');
+
+  // Cuando hago click en cada li, añade el párrafo
+  movie1.addEventListener("click", (event) => {
+    console.log("Hago click en ", event.currentTarget.innerHTML);
+    p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
+  });
+  movie2.addEventListener("click", (event) => {
+    console.log("Hago click en " + event.currentTarget.innerHTML);
+    p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
+  });
+  movie3.addEventListener("click", (event) => {
+    console.log("Hago click en ", event.currentTarget.innerHTML);
+    p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
+  });
+  movie4.addEventListener("click", (event) => {
+    console.log("Hago click en ", event.currentTarget.innerHTML);
+    p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
+  });
+  movie5.addEventListener("click", (event) => {
+    console.log("Hago click en ", event.currentTarget.innerHTML);
+    p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
+  });
 });
 
-const movie = document.querySelector('.movie');
-const p = document.querySelector('.js-paragraph');
 
-// Cuando hago click en cada li, añade el párrafo
-movie.addEventListener("click", (event) => {
-  console.log("Hago click en movie");
-  p.innerHTML = `Has seleccionado la película ${event.currentTarget.innerHTML}`;
-});
