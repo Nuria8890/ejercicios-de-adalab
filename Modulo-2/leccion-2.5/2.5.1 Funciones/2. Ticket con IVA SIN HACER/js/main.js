31e3f02212029218903ae033815450810387ba95
Nuria Campo <nuria.campo7@gmail.com>
2024-10-17 15:58:35 +0200

    Ejercicio 2.5.1-2 Ticket con IVA

--- Modulo-2/leccion-2.5/2.5.1 Funciones/2. Ticket con IVA SIN HACER/js/main.js
+++ Modulo-2/leccion-2.5/2.5.1 Funciones/2. Ticket con IVA SIN HACER/js/main.js
@@ -1,4 +1,13 @@
 'use strict';
-// La página web carga mejor, es más obtima
-// Resalta más los errores
-// 
+
+function calculaIVA (a) {
+  const IVA = a * 0.21;
+  const precioSinIva = a - IVA;
+
+  return console.log(`El precio total es ${a}, el precio sin IVA es ${precioSinIva.toFixed(2)}, y el IVA en sí es ${IVA.toFixed(2)}`)
+}
+
+const precio1 = 25;
+const precio2 = 36.28;
+calculaIVA(precio1);
+calculaIVA(precio2);
