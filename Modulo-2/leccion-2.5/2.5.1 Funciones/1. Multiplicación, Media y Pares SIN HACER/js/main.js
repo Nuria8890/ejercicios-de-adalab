e3c7237a519e3cb81dee01923257713564219bd3
Nuria Campo <nuria.campo7@gmail.com>
2024-10-17 15:49:46 +0200

    Ejercicio 2.5.1-1 Multiplicación, media y pares

--- Modulo-2/leccion-2.5/2.5.1 Funciones/1. Multiplicación, Media y Pares SIN HACER/js/main.js
+++ Modulo-2/leccion-2.5/2.5.1 Funciones/1. Multiplicación, Media y Pares SIN HACER/js/main.js
@@ -1,4 +1,44 @@
 'use strict';
-// La página web carga mejor, es más obtima
-// Resalta más los errores
-// 
+
+// Crea una función que reciba como argumentos dos valores y devuelva como valor de retorno la multiplicación de ambos. Haz tres pruebas con distintos números para comprobar que funciona correctamente y muestra el resultado en la consola usando console.log().
+
+function multiplica(a, b) {
+  return console.log(`La multiplicación de ${a}*${b} es`, a*b);
+}
+
+const number1 = 2;
+const number2 = 4;
+const number3 = 5;
+const number4 = 8;
+
+multiplica(number1,number2); // 8
+multiplica(number3,number4); // 40
+
+// Crea una función con 4 parámetros numéricos que devuelva como valor la media de todos ellos. Haz tres pruebas con distintos números para comprobar que funciona correctamente y muestra el resultado en la consola.
+
+function media(c, d, e, f) {
+  const suma = c + d + e + f;
+  return console.log(`La media de ${c}, ${d}, ${e} y ${f} es`, suma / 4);
+}
+
+const number5 = 3;
+const number6 = 6;
+const number7 = 7;
+const number8 = 9;
+
+media(number1,number2,number3,number4);
+media(number5,number6,number7,number8);
+
+// Crear una función que reciba por parámetro un número y devuelva true si es par y false si es impar. Ejecútala e imprime el resultado para comprobar que funciona.
+
+function isPar(a) {
+  const divide2 = a / 2;
+  if (Number.isInteger(divide2)){
+    return console.log(`¿El número ${a} es par?`, true);
+  } else {
+    return console.log(`¿El número ${a} es par?`, false);
+  }
+}
+
+isPar(number6);
+isPar(number8);
