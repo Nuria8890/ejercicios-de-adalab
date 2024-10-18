1cfe23f3281dc3f58d4b7dcc11f64444f9c7c464
Nuria Campo <nuria.campo7@gmail.com>
2024-10-18 18:53:39 +0200

    Ejercicio 2.2.3-2 Estilo para un botón con otra solución...

--- Modulo-2/leccion-2.2/2.2.3 Información de eventos/2. Estilo para un boton NO FUNCIONA/js/main.js
+++ Modulo-2/leccion-2.2/2.2.3 Información de eventos/2. Estilo para un boton NO FUNCIONA/js/main.js
@@ -2,29 +2,42 @@
 
 // Con una función para cada botón, funciona:
 
-const btn1 = document.querySelector('.btn-1');
+// const btn1 = document.querySelector('.js-btn-1');
 
-const btn2 = document.querySelector('.btn-2');
+// const btn2 = document.querySelector('.js-btn-2');
 
-btn1.addEventListener("click", () => {
-  btn1.classList.toggle('btn-click');
-});
+// btn1.addEventListener("click", () => {
+//   btn1.classList.toggle('btn-click');
+// });
 
-btn2.addEventListener("click", () => {
-  btn2.classList.toggle('btn-click');
-});
+// btn2.addEventListener("click", () => {
+//   btn2.classList.toggle('btn-click');
+// });
 
 
 // Con una función para los dos botones, NO funciona:
 
-// const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.js-btn');
 
 // btn.addEventListener("click", (event) => {
 //   console.log('hago click');
 
-//   const btnClick = event.currentTarget;
+//   const btnClick = event.target;
 //   btnClick.classList.toggle('btn-click');
 // })
+
+
+// Con una función para los dos botones, creando un div que envuelva ambos, también escucha el click en el div y me cambia el estilo del div.
+
+const buttonsContainer = document.querySelector('.js-buttons-container');
+
+buttonsContainer.addEventListener("click", (event) => {
+  console.log('hago click en', event.target);
+
+  const btnClick = event.target;
+  btnClick.classList.toggle('btn-click');
+})
+
 
 
 
