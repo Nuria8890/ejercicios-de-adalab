29ff1d1d7b98d4a35569ab049db7825121fd3f26
Nuria Campo <nuria.campo7@gmail.com>
2024-10-23 19:41:32 +0200

    Ejercicio 2.9.1-1 Practicando

---
+++ Modulo-2/leccion-2.9/2.9.1 Bucles/1. Practicando/js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+console.log('Ejercicio 1');
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Voy por la vuelta ", i);
+}
+
+
+console.log('Ejercicio 2');
+
+let acc = 0;
+
+for (let i = 0; i < 10; i++) {
+  acc = acc + 2;
+}
+console.log('El resultado es:', acc);
+
+
+
+
+// CÉSAR
+// function bucle(item) {
+//   // hago cosos
+// }
+// const frutas = ["pera", "platano"]
+// for (const fruta of frutas) {
+//   bucle(fruta)
+// }
+
+// frutas.forEach(bucle)
+
+// function buscarPera(frutas) {
+//   for (const fruta of frutas) {
+//     if (fruta === "pera") {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
