6a717f06f237a7653ea58f3f0a54a3e7c9102e8f
Nuria Campo <nuria.campo7@gmail.com>
2024-10-29 19:20:08 +0100

    Ejercicio 2.12.1-3 Gracias por confirar en nosotros

---
+++ Modulo-2/leccion-2.12/2.12.1 Map/3. Gracias por confiar en nosotros/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const users = [
+  {name: 'María', isPremium: false},
+  {name: 'Lucía', isPremium: true},
+  {name: 'Susana', isPremium: true},
+  {name: 'Rocío', isPremium: false},
+  {name: 'Inmaculada', isPremium: false},
+];
+
+const saludos = users.map((user) => {
+  if(user.isPremium === true) {
+    return 'Bienvenida ' + user.name + '. Gracias por confiar en nosotros.'
+  } else {
+    return 'Bienvenida ' + user.name
+  }
+})
+console.log(saludos);
