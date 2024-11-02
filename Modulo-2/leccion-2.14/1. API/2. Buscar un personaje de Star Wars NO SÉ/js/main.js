56a65db314f2d772b3f74a7e3b2f91f7c50cd504
Nuria Campo <nuria.campo7@gmail.com>
2024-11-02 15:47:49 +0100

    Ejercicio 2.14.1-2 Buscar un personaje de Star Wars TERMINADO

--- Modulo-2/leccion-2.14/1. API/2. Buscar un personaje de Star Wars NO SÉ/js/main.js
+++ Modulo-2/leccion-2.14/1. API/2. Buscar un personaje de Star Wars NO SÉ/js/main.js
@@ -10,50 +10,47 @@ const button = document.querySelector(".js-button");
 const input = document.querySelector(".js-input");
 const ul = document.querySelector(".js-list");
 
-// const searchOnServer = () => {
-//   fetch("https://swapi.py4e.com/api/people/").then((response) => {
-//     console.log("response.json() es:", response.json());
-//     return response.json();
-//   });
-// };
-
-// const getData = () => {
-//   const promises = [];
-//   for (let i = 0; i < promises.length; i++) {
-//     promises.push(searchOnServer());
-//   }
-//   console.log("promises es:", promises);
-//   Promise.all(promises).then((responses) => {
-//     for (let i = 0; i < responses.length; i++) {
-//       const nameCharacter = input.value;
-//       //     ul.innerHTML = `
-//       // <li>Nombre:${} Género:${}</li>
-//       // `
-//     }
-//   });
-// };
-
 const handleClick = (event) => {
   event.preventDefault();
-  console.log("click");
-  datosLukeSkywalker();
-  // getData();
+
+  const name = input.value;
+
+  fetch(`https://swapi.py4e.com/api/people/?search=${name}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data es:", data);
+
+      // Cuántas páginas hay
+      const pages = Math.ceil(data.count / 10);
+
+      for (let i = 1; i <= pages; i++) {
+        fetch(`https://swapi.py4e.com/api/people/?search=${name}&page=${i}`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            for (let i = 0; i < data.results.length; i++) {
+              ul.innerHTML += `<li>El nombre del personaje es: ${data.results[i].name}, y el género es ${data.results[i].gender}</li>`;
+            }
+          });
+      }
+    });
 };
 
 button.addEventListener("click", handleClick);
 
+// Pinta a Luke
 const datosLukeSkywalker = () => {
   fetch("https://swapi.py4e.com/api/people/1/")
     .then((response) => {
-      console.log("response es:", response);
-      console.log("response.json() es:", response.json());
-      // return response.json();
-      return response;
+      return response.json();
     })
     .then((data) => {
       console.log("data es:", data);
       ul.innerHTML = `
-    <li>${data.url}</li>
+    <li>${data.name}</li>
     `;
     });
 };
