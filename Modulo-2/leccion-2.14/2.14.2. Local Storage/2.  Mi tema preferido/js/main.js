1368dcd53c59388ea75dd360df20c8e4738bc23f
Nuria Campo <nuria.campo7@gmail.com>
2024-11-02 11:47:24 +0100

    Ejercicio 2.14.2-2 Mi tema preferido -- cambia el tema en la página

--- Modulo-2/leccion-2.14/2.14.2. Local Storage/2.  Mi tema preferido/js/main.js
+++ Modulo-2/leccion-2.14/2.14.2. Local Storage/2.  Mi tema preferido/js/main.js
@@ -1,1 +1,28 @@
 "use strict";
+/* Cuando la usuaria seleccione uno u otro input
+    - aplicaremos la clase correspondiente al main
+    - guardar esa selección en localStorage
+  
+  Cuando la página se recargue
+    - buscar en localSotage la selección
+    - aplicarla al main
+*/
+
+const formulario = document.querySelector(".js-form");
+const paginaCompleta = document.querySelector(".js-main");
+
+const changeTheme = (event) => {
+  if (event.target.value === "claro") {
+    paginaCompleta.classList.add("fondoClaro");
+    paginaCompleta.classList.remove("fondoOscuro");
+  } else {
+    paginaCompleta.classList.remove("fondoClaro");
+    paginaCompleta.classList.add("fondoOscuro");
+  }
+};
+
+const handleChange = (event) => {
+  changeTheme(event);
+};
+
+formulario.addEventListener("change", handleChange);
