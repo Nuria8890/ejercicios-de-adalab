cc2ce04367a9f74f34971e33637fa48ec095ceed
Nuria Campo <nuria.campo7@gmail.com>
2024-11-04 19:27:52 +0100

    Ejercicio 2.15.2-1 1,2,3 lo hacemos otra vez

---
+++ Modulo-2/leccion-2.15/2.15.2 Crear, añadir y eliminar/1.  1, 2, 3, lo hacemos otra vez/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const numbers = [1, 2, 3];
+const list = document.querySelector(".js-list");
+
+for (const number of numbers) {
+  const newItem = document.createElement("li");
+  const newContent = document.createTextNode(number);
+  newItem.appendChild(newContent);
+  console.log(newItem);
+  list.appendChild(newItem);
+}
