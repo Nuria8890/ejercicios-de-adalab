5420406b25a989acba8dab0dce3f7e4685603a61
Nuria Campo <nuria.campo7@gmail.com>
2024-11-14 16:20:22 +0100

    Ejercicio 3.2.2-1 template Muestra la tecla pulsada en la consola

---
+++ Modulo-3/leccion 3.2/3.2.2 Eventos/1. Muestra la tecla pulsada en la consola/src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    // fragment
+    <>
+      <header className="header" id="header">
+        <h1 className="header__title">Título de la página</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
