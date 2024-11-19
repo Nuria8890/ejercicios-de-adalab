e08cd9d0ebac34f4d8d97fb0ca578718074825dc
Nuria Campo <nuria.campo7@gmail.com>
2024-11-19 19:54:05 +0100

    Ejercicio 3.5.2 Arrays y Estado NO FUNCIONA

---
+++ Modulo-3/leccion 3.5./3.5.2 Arrays y Estado NO FUNCIONA/src/components/App.jsx
@@ -0,0 +1,57 @@
+import "../styles/App.scss";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { task: "Comprar harina, jamón y pan rallado", completed: true, id: 1 },
+    { task: "Hacer croquetas ricas", completed: true, id: 2 },
+    { task: "Ir a la puerta de un gimnasio", completed: false, id: 3 },
+    {
+      task: "Comerme las croquetas mirando a la gente que entra en el gimnasio",
+      completed: false,
+      id: 4,
+    },
+  ]);
+
+  const handleClick = (event) => {
+    const taskClickedIndex = event.currentTarget.id;
+
+    const foundTask = tasks.find((task) => {
+      task.id === taskClickedIndex;
+      console.log("task.id", task.id);
+      console.log("taskClickedIndex", taskClickedIndex);
+    });
+    console.log(foundTask);
+    foundTask.completed = !foundTask.completed;
+
+    setTasks([...tasks]);
+  };
+
+  const renderList = () => {
+    const list = tasks.map((task) => {
+      return (
+        <li
+          className={task.completed ? "strike" : ""}
+          key={task.id}
+          id={task.id}
+          onClick={handleClick}
+        >
+          {task.task}
+        </li>
+      );
+    });
+
+    return <ol className="listOl">{list}</ol>;
+  };
+
+  return (
+    <>
+      <header className="header" id="header">
+        <h1 className="header__title">Mi lista de tareas</h1>
+      </header>
+      <main>{renderList()}</main>
+    </>
+  );
+}
+
+export default App;
