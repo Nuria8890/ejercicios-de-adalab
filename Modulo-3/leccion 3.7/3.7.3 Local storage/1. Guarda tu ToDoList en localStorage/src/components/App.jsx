cf1d772cc58643fa32ae60a886d1f2e2ce1e759c
Nuria Campo <nuria.campo7@gmail.com>
2024-11-24 13:01:41 +0100

    Ejercicio 3.7.3-1 Guarda tu ToDo list en localStorage

---
+++ Modulo-3/leccion 3.7/3.7.3 Local storage/1. Guarda tu ToDoList en localStorage/src/components/App.jsx
@@ -0,0 +1,56 @@
+import "../styles/App.scss";
+import { useState, useEffect } from "react";
+import localStorage from "../services/localStorage";
+
+function App() {
+  // Estados
+  const [tasks, setTasks] = useState(localStorage.get("tasks", ""));
+
+  // useEffect
+  useEffect(() => {
+    localStorage.set("tasks", tasks);
+    console.log("Han cambiado las tareas");
+  }, [tasks]);
+
+  // Eventos
+  const handleClick = (event) => {
+    const taskClickedIndex = event.currentTarget.id;
+
+    const foundTask = tasks.find((task) => {
+      return task.id === parseInt(taskClickedIndex);
+    });
+
+    foundTask.completed = !foundTask.completed;
+
+    setTasks([...tasks]);
+  };
+
+  // Funciones
+  const renderList = () => {
+    const list = tasks.map((task) => {
+      return (
+        <li
+          className={task.completed ? "strike" : ""}
+          key={task.id}
+          id={task.id}
+          onClick={handleClick}
+        >
+          {task.task}
+        </li>
+      );
+    });
+
+    return <ol className="listOl">{list}</ol>;
+  };
+
+  return (
+    <>
+      <header className="header" id="header">
+        <h1 className="header__title">Mi lista de tareas</h1>
+      </header>
+      <main>{renderList()}</main>
+    </>
+  );
+}
+
+export default App;
