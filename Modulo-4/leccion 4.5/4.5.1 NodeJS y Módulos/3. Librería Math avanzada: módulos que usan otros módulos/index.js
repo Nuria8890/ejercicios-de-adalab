fa5eef13bed00d546de109fa1599987e9de16fee
Nuria Campo <nuria.campo7@gmail.com>
2024-12-09 17:46:15 +0100

    Ejercicio 4.5.1-3 Librería Math avanzada: módulos que usan otros módulos

---
+++ Modulo-4/leccion 4.5/4.5.1 NodeJS y Módulos/3. Librería Math avanzada: módulos que usan otros módulos/index.js
@@ -0,0 +1,4 @@
+let operaciones = require("./math");
+console.log(operaciones);
+console.log(operaciones.suma(4, 6));
+console.log(operaciones.resta(4, 6));
---
+++ Modulo-4/leccion 4.5/4.5.1 NodeJS y Módulos/3. Librería Math avanzada: módulos que usan otros módulos/math.js
@@ -0,0 +1,7 @@
+let suma = require("./math-add");
+let resta = require("./math-sub");
+
+module.exports = {
+  suma: suma,
+  resta: resta,
+};
